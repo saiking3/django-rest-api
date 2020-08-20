854766c3c754acc2001a397152cae7d6bf9f58db
saiking131 <saiking131@gmail.com>
2020-08-20 15:32:00 +0530

    function based views

---
+++ userdetails/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import article_list, article_details
+
+urlpatterns = [
+    path('article/', article_list),
+    path('details/<int:pk>', article_details),
+]
