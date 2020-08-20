77c0b3a71a4cfcb6cdeb7a6c115779e6c3c19c4e
saiking131 <saiking131@gmail.com>
2020-08-20 16:11:11 +0530

    api view decorator

--- userdetails/serializers.py
+++ userdetails/serializers.py
@@ -5,4 +5,5 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['title', 'author', 'email']
+        #fields = ['title', 'author', 'email']#
+        fields='__all__'
