7fc86c7c0cbbdfcbc7fc211ad89ba324b923168d
saiking131 <saiking131@gmail.com>
2020-08-21 12:15:19 +0530

    generic views and mixins

--- userdetails/urls.py
+++ userdetails/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import article_list, article_details, ArticleAPIView, ArticleDetails
+from .views import article_list, article_details, ArticleAPIView, ArticleDetails, GenericAPIView
 
 urlpatterns = [
     #path('article/', article_list),
     path('article/', ArticleAPIView.as_view()),
+    path('generics/article/<int:id>/', GenericAPIView.as_view()),
     #path('details/<int:pk>', ),
-    path('details/<int:id>', ArticleDetails.as_view()),
+    path('details/<int:id>/', ArticleDetails.as_view()),
 ]
--- userdetails/views.py
+++ userdetails/views.py
@@ -11,10 +11,13 @@ from rest_framework.response import Response
 from rest_framework import status
 #importing apiview 
 from rest_framework.views import APIView
+from django.views.decorators.csrf import csrf_exempt
+# generic views and mixins
+from rest_framework import generics
+from rest_framework import mixins
+
 
 # Create your views here.
-from django.views.decorators.csrf import csrf_exempt
-
 @api_view(['GET','POST'])
 def article_list(request):
     
@@ -98,3 +101,29 @@ class ArticleDetails(APIView):
         article = self.get_object(id)
         article.delete()
         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+#--------Generic API Views and Mixins-------#
+
+class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, 
+                    mixins.CreateModelMixin,mixins.UpdateModelMixin, 
+                    mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+
+    lookup_field = 'id'
+
+    def get(self, request, id=None):
+
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+
+    def post(self, request, id):
+        return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    def delete(self, request, id):
+        return self.destroy(request, id)
